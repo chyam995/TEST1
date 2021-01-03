7f5a45e902c8476cebc6731b26879e3982772d7d
chyam <chyam995@gmail.com>
2021-01-03 14:49:03 +0300

    Исправлено задание 3

--- Задание 1-3/src/main/java/logic/stringArray.java
+++ Задание 1-3/src/main/java/logic/stringArray.java
@@ -1,5 +1,6 @@
 package logic;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class stringArray {
@@ -11,13 +12,34 @@ public class stringArray {
         int i = scanner.nextInt();
         String[] array = new String[i];
         String findword = "";
+        HashSet<String> maxwords = new HashSet<String>();
         for (int j = 0; j < i; j++) {
             System.out.println("Введите слово ");
             array[j] = scanner.next();
             if (array[j].length() >= findword.length()) findword = array[j];
         }
+        for (int j = 0; j < i; j++)
+        {
+            if(array[j].length()==findword.length())
+            {
+                maxwords.add(array[j]);
+            }
 
-        System.out.println("Самое длинное слово в массиве :" + findword);
+        }
+        if(maxwords.size()>1)
+        {
+            System.out.println("Самые длинные слова в массиве :");
+            for(String Maxword:maxwords )
+            {
+                System.out.println(Maxword);
+            }
+        }
+        else {
+            for(String Maxword:maxwords )
+            {
+                System.out.println("Самое длинное слово в массиве :" + Maxword);
+            }
+        }
         System.out.println("Размер максимального слова в массиве равен:" + findword.length());
     }
 }
